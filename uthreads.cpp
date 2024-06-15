940f73835aca53b5ee6dcff3254c4a1589698830
gugiel <guy1.lidor@gmail.com>
2024-06-15 14:32:22 +0300

    uthreads.cpp 1

---
+++ uthreads.cpp
@@ -0,0 +1,14 @@
+//
+// Created by TLP-258 on 15/06/2024.
+//
+
+#include <memory>
+#include "uthreads.h"
+#include "ThreadScheduler.h"
+
+std::unique_ptr<ThreadScheduler> scheduler;
+
+int uthread_get_quantums(int tid)
+{
+
+}
